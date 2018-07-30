1cf2b64ea8de60bbc8f8683e66d6bd2c6cc02678
Burak Arslan <burak.arslan@arskom.com.tr>
2018-07-30 12:34:06 +0300

    move to own package, modernize packaging to some extent

---
+++ pyshaper/__init__.py
@@ -0,0 +1,4 @@
+
+__version__ = '0.1.3-dev'
+
+from pyshaper.main import *
--- setup.py
+++ setup.py
@@ -1,31 +1,34 @@
-from distutils.core import setup
 
-# set this to the base path of your manpages
-manpagesBaseDir = "/usr/share/man"
+import os, re
 
-import sys, os
+from setuptools import setup
+from setuptools import find_packages
 
-if os.getuid() != 0:
-    print "Sorry, you must be root to install this program"
-    sys.exit(1)
+
+PACKAGE_NAME = 'pyshaper'
+
+v = open(os.path.join(os.path.dirname(__file__), PACKAGE_NAME, '__init__.py'))
+VERSION = re.compile(r'.*__version__ = ["\'](.*?)["\']', re.S) \
+                                                       .match(v.read()).group(1)
+
+MANPAGES_BASE_DIR = "/usr/share/man"
 
 setup(
-  name="pyshaper",
-  version='0.1.1',
-  description='Dynamic traffic-shaping app for Linux',
-  author='David McNab',
-  author_email='david@freenet.org.nz',
-  url='http://www.freenet.org.nz/python/pyshaper',
-  packages=[
-    ],
-  py_modules = [
-    ],
-  scripts=[
-    "pyshaper",
-    ],
-  data_files=[
-    ('/etc/pyshaper', ['pyshaper.conf', 'pyshaper.conf.readme']),
-    (manpagesBaseDir+'/man8', ['pyshaper.8', 'pyshaper.conf.8']),
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description='Dynamic traffic-shaping app for Linux',
+    author='David McNab',
+    author_email='david@freenet.org.nz',
+    url='http://www.freenet.org.nz/python/pyshaper',
+    packages=find_packages(),
+    install_requires=['Pmw'],
+    entry_points=dict(
+        console_scripts=[
+            "pyshaper=pyshaper.main:main",
+        ],
+    ),
+    data_files=[
+        ('/etc/pyshaper', ['pyshaper.conf', 'pyshaper.conf.readme']),
+        (MANPAGES_BASE_DIR + '/man8', ['pyshaper.8', 'pyshaper.conf.8']),
     ],
 )
-
